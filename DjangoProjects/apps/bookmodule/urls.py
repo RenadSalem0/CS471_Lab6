9f7c730fc17afcd62a6144c586b51d50b8ca0df2
RenadSalem0 <Renad9789@gmail.com>
2026-04-05 10:23:05 +0300

    Add files via upload

---
+++ DjangoProjects/apps/bookmodule/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+path('', views.index, name= "books.index"),
+path('list_books/', views.list_books, name= "books.list_books"),
+path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+path('aboutus/', views.aboutus, name="books.aboutus"),
+path("html5/links", views.html5_links, name="html5_links"),
+path("html5/text/formatting", views.html5_text_formatting, name="html5_text_formatting"),
+path("html5/listing", views.html5_listing, name="html5_listing"),
+path("html5/tables", views.html5_tables, name="html5_tables"),
+path('search', views.search, name='search'),
+]
---
+++ DjangoProjects/apps/bookmodule/views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+'''def index(request):
+   # return HttpResponse("Hello, world!")
+   name = request.GET.get("name") or "world!" #add this line
+   #return HttpResponse("Hello, "+name) #replace the word “world!” with the variable nam
+   #return render(request, "bookmodule/index.html")
+   return render(request, "bookmodule/index.html" , {"name": name}) '''
+
+def index2(request, val1 = 0): #add the view function (index2)
+   return HttpResponse("value1 = "+str(val1))
+
+def viewbook(request, bookId):
+# assume that we have the following books somewhere (e.g. database)
+  book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+  book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
+  targetBook = None
+  if book1['id'] == bookId: targetBook = book1
+  if book2['id'] == bookId: targetBook = book2
+  context = {'book':targetBook} # book is the variable name accessible by the template
+  return render(request, 'bookmodule/show.html', context)
+def index(request):
+  return render(request, "bookmodule/index.html")
+def list_books(request):
+  return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+  return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+  return render(request, 'bookmodule/aboutus.html')
+
+def html5_links(request):
+    return render(request, "bookmodule/html5_links.html")
+
+def html5_text_formatting(request):
+    return render(request, "bookmodule/html5_text_formatting.html")
+
+def html5_listing(request):
+    return render(request, "bookmodule/html5_listing.html")
+
+def html5_tables(request):
+    return render(request, "bookmodule/html5_tables.html")
+
+def __getBooksList():
+    book1 = {'id':12344321, 'title':'Continuous Delivery', 'author':'J.Humble and D. Farley'}
+    book2 = {'id':56788765, 'title':'Reversing: Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    book3 = {'id':43211234, 'title':'The Hundred-Page Machine Learning Book', 'author':'Andriy Burkov'}
+    return [book1, book2, book3]
+
+def search(request):
+    if request.method == "POST":
+        string = request.POST.get('keyword').lower()
+        isTitle = request.POST.get('option1')
+        isAuthor = request.POST.get('option2')
+
+        books = __getBooksList()
+        newBooks = []
+
+        for item in books:
+            contained = False
+
+            if isTitle and string in item['title'].lower():
+                contained = True
+
+            if not contained and isAuthor and string in item['author'].lower():
+                contained = True
+
+            if contained:
+                newBooks.append(item)
+
+        return render(request, 'bookmodule/bookList.html', {'books': newBooks})
+
+    return render(request, 'bookmodule/search.html')
